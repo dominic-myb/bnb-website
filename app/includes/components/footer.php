73bf1ab2e886bd0fd8bec0a6bfd657e985e6ea25
dominic-myb <desguerra246@gmail.com>
2024-01-23 19:20:07 +0800

    Added Source

--- app/includes/components/footer.php
+++ app/includes/components/footer.php
@@ -1,16 +1,17 @@
 <div class="footer">
-        <div class="footer-box">
-            <div class="social-media">
-                <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                <a href="#"><i class="fa-brands fa-youtube"></i></a>
-            </div>
-            <div class="copyright">
-                <label>Copyright &copy; 2023</label>
-            </div>
-            <div class="brand">
-                <label>Café <span>BLK & BRWN</span></label>
-            </div>
+    <div class="footer-box">
+        <div class="social-media">
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+        </div>
+        <div class="copyright">
+            <label>Copyright &copy; 2023</label>
+        </div>
+        <div class="brand">
+            <label>Café <span>BLK & BRWN</span></label>
         </div>
     </div>
+</div>
+<script src="assets/js/main.js" defer></script>
