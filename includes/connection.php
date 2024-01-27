9a68bc394ce4e4a0d6667a5fe0fc1a200261d915
dominic-myb <desguerra246@gmail.com>
2024-01-28 03:37:59 +0800

    Add Error Callback

--- includes/connection.php
+++ includes/connection.php
@@ -3,7 +3,8 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "bnb_database";
+
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
-	die("Connection Failed!");
+	die("Connection Failed!").mysqli_error($con);
 }
