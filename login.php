f3b2f76374a56d3dcc2a2cb2737a9e1cf1ff4759
dominic-myb <desguerra246@gmail.com>
2024-01-30 02:23:43 +0800

    Update include directories

--- login.php
+++ login.php
@@ -1,101 +1,76 @@
 <?php
 session_start();
-      include("includes/connection.php");
-      include("includes/function.php");
-      
-      if($_SERVER['REQUEST_METHOD'] == "POST")
-      {
+include("app/includes/components/connection.php");
+include("app/includes/components/function.php");
 
-        $cus_username = $_POST['username'];
-        $cus_password = $_POST['password'];
-        $admin_username = $_POST['username'];
-        $admin_password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-        // if(!empty($admin_username) && !empty($admin_password))
-        // {
-        //   $query = "select * from admin where admin_username = '$admin_username'limit 1";
-        //   $result = mysqli_query($con, $query);
-        //   if($result)
-        //   {
-        //     if($result && mysqli_num_rows($result) > 0)
-        //     {
-        //       $user_data = mysqli_fetch_assoc($result);
-        //       if($user_data['admin_password'] === $admin_password)
-        //       {
-        //         header("location: admin.php");
-        //         die;
-        //       }
-        //     }
-           
-        //   }
+  $cus_username = $_POST['username'];
+  $cus_password = $_POST['password'];
+  $admin_username = $_POST['username'];
+  $admin_password = $_POST['password'];
 
-        if(!empty($cus_username) && !empty($cus_password) && !is_numeric($cus_username))
-        {
-          $query = "select * from customer_tbl where cus_username = '$cus_username' limit 1";
-          $result = mysqli_query($con, $query);
-          if($result)
-          {
-            if($result && mysqli_num_rows($result) > 0)
-            {
-              $user_data = mysqli_fetch_assoc($result);
-              if($user_data['cus_password'] === $cus_password)
-              {
-                $_SESSION['customer_id'] = $user_data['customer_id'];
-                header("location: index.php");
-                die;
-              }
-            }
-            else
-            {
-              $query = "select * from admin where admin_username = '$admin_username'limit 1";
-              $result = mysqli_query($con, $query);
-              if($result)
-              {
-                if($result && mysqli_num_rows($result) > 0)
-               {
-                $user_data = mysqli_fetch_assoc($result);
-                if($user_data['admin_password'] === $admin_password)
-                {
-                  header("location: admin.php");
-                  die;
-                }
-               }
-              }
+  if (!empty($cus_username) && !empty($cus_password) && !is_numeric($cus_username)) {
+    $query = "SELECT * FROM customer_tbl WHERE username = '$cus_username' LIMIT 1";
+    $result = mysqli_query($con, $query);
+    if ($result) {
+      if ($result && mysqli_num_rows($result) > 0) {
+        $user_data = mysqli_fetch_assoc($result);
+        if ($user_data['password'] === $cus_password) {
+          $_SESSION['customer_id'] = $user_data['customer_id'];
+          header("location: index.php");
+          die;
+        }
+      } else {
+        $query = "SELECT * FROM admin WHERE admin_username = '$admin_username' LIMIT 1";
+        $result = mysqli_query($con, $query);
+        if ($result) {
+          if ($result && mysqli_num_rows($result) > 0) {
+            $user_data = mysqli_fetch_assoc($result);
+            if ($user_data['admin_password'] === $admin_password) {
+              header("location: admin.php");
+              die;
             }
           }
-          header("location: error.php");
-        }
-        else
-        {
-          echo"PLEASE ENTER VALID INFORMATiON";
         }
       }
+    } {
+      echo '<script>
+        window.alert("Incorrect Username or Password!")
+        window.location="login.php"
+      </script>';
+    }
+  } else {
+    echo '<script>
+    window.alert("Please Enter Valid Input!")
+    window.location="login.php"
+    </script>';
+  }
+}
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <link rel="icon" type="image/png" href="img/tab-icon.png">
-  <link rel="stylesheet" type="text/css" href="assets/css/loginStyle.css">
-  <title>Café BLK & BRWN - Login</title>
-</head>
+
+<?php
+$title = "- Login";
+include("app/includes/html/login.head.php");
+?>
+
 <body>
   <div class="container">
     <div class="brand">
-        <h2>Café <span>BLK & BRWN</span></h2>
+      <h2>Café <span>BLK & BRWN</span></h2>
     </div>
     <!-- <h2>Café BLK & BRWN</h2> -->
     <form action="login.php" method="post">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required>
-
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required>
-
-      <input type="submit" value="Login">
+      <input type="text" id="username" name="username" placeholder="Username" required>
+      <input type="password" id="password" name="password" placeholder="Password" required>
+      <input type="submit" value="Login"><br>
       <hr>
-      <button><a class="create-acc" href="register.php">Create new account</a></button>
-      
+      <p class="create-acc">New to Black & Brown? &nbsp<a href="form.php">Create Account</a></p>
+
     </form>
   </div>
 </body>
+
 </html>
