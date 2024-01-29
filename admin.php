22f74d0f60a0ea1c39402a525ce57fcec81a8d79
dominic-myb <desguerra246@gmail.com>
2024-01-30 02:19:57 +0800

    Added html head and foot

--- admin.php
+++ admin.php
@@ -1,25 +1,16 @@
-<?php
-session_start();
-include("includes/connection.php");
-?>
+<?php include("app/includes/components/connection.php"); ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Café BLK & BRWN - Admin</title>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-       <link rel="stylesheet" href="./assets/css/adminStyle.css"></link>
-       <link rel="icon" type="image/png" href="img/tab-icon.png">
-  </head>
-</head>
-<body >
-    
-        <?php
-            include "./includes/adminHeader.php";
-            include "./includes/sidebar.php";
-        ?>
+<?php
+$title = "Admin";
+include("app/includes/html/admin.head.php");
+?>
+
+<body>
+    <?php
+    include "app/includes/html/admin.head.php";
+    include "app/includes/html/admin.sidebar.php";
+    ?>
 
     <div id="main-content" class="container allContent-section py-4">
         <div class="row">
@@ -28,39 +19,40 @@ include("includes/connection.php");
                     <i class="fa fa-users  mb-2" style="font-size: 70px;"></i>
                     <h4 style="color:white;">Total Users</h4>
                     <h5 style="color:white;">
-                    <?php
-                        $sql="SELECT * from customer_tbl";
-                        $result=$con-> query($sql);
-                        $count=0;
-                        if ($result-> num_rows > 0){
-                            while ($row=$result-> fetch_assoc()) {
-                    
-                                $count=$count+1;
+                        <?php
+                        $sql = "SELECT * from customer_tbl";
+                        $result = $con->query($sql);
+                        $count = 0;
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+
+                                $count = $count + 1;
                             }
                         }
                         echo $count;
-                    ?></h5>
+                        ?>
+                    </h5>
                 </div>
             </div>
             <div class="col-sm-3">
-            <div class="card">
+                <div class="card">
                     <i class="fa fa-th mb-2" style="font-size: 70px;"></i>
                     <h4 style="color:white;">Total Products</h4>
                     <h5 style="color:white;">
-                    <?php
-                       
-                       $sql="SELECT * from product_tbl";
-                       $result=$con-> query($sql);
-                       $count=0;
-                       if ($result-> num_rows > 0){
-                           while ($row=$result-> fetch_assoc()) {
-                   
-                               $count=$count+1;
-                           }
-                       }
-                       echo $count;
-                   ?>
-                   </h5>
+                        <?php
+
+                        $sql = "SELECT * from product_tbl";
+                        $result = $con->query($sql);
+                        $count = 0;
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+
+                                $count = $count + 1;
+                            }
+                        }
+                        echo $count;
+                        ?>
+                    </h5>
                 </div>
             </div>
             <div class="col-sm-3">
@@ -68,51 +60,42 @@ include("includes/connection.php");
                     <i class="fa fa-list mb-2" style="font-size: 70px;"></i>
                     <h4 style="color:white;">Total orders</h4>
                     <h5 style="color:white;">
-                    <?php
-                       
-                       $sql="SELECT * from order_tbl";
-                       $result=$con-> query($sql);
-                       $count=0;
-                       if ($result-> num_rows > 0){
-                           while ($row=$result-> fetch_assoc()) {
-                   
-                               $count=$count+1;
-                           }
-                       }
-                       echo $count;
-                   ?>
-                   </h5>
+                        <?php
+
+                        $sql = "SELECT * from order_tbl";
+                        $result = $con->query($sql);
+                        $count = 0;
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+
+                                $count = $count + 1;
+                            }
+                        }
+                        echo $count;
+                        ?>
+                    </h5>
                 </div>
             </div>
         </div>
-        
     </div>
-       
-            
-        <?php
-            if (isset($_GET['category']) && $_GET['category'] == "success") {
-                echo '<script> alert("Category Successfully Added")</script>';
-            }else if (isset($_GET['category']) && $_GET['category'] == "error") {
-                echo '<script> alert("Adding Unsuccess")</script>';
-            }
-            if (isset($_GET['size']) && $_GET['size'] == "success") {
-                echo '<script> alert("Size Successfully Added")</script>';
-            }else if (isset($_GET['size']) && $_GET['size'] == "error") {
-                echo '<script> alert("Adding Unsuccess")</script>';
-            }
-            if (isset($_GET['variation']) && $_GET['variation'] == "success") {
-                echo '<script> alert("Variation Successfully Added")</script>';
-            }else if (isset($_GET['variation']) && $_GET['variation'] == "error") {
-                echo '<script> alert("Adding Unsuccess")</script>';
-            }
-        ?>
 
-
-    <script type="text/javascript" src="./assets/js/ajaxWork.js"></script>    
-    <script type="text/javascript" src="./assets/js/script.js"></script>
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+    <?php
+    if (isset($_GET['category']) && $_GET['category'] == "success") {
+        echo '<script> alert("Category Successfully Added")</script>';
+    } else if (isset($_GET['category']) && $_GET['category'] == "error") {
+        echo '<script> alert("Adding Unsuccess")</script>';
+    }
+    if (isset($_GET['size']) && $_GET['size'] == "success") {
+        echo '<script> alert("Size Successfully Added")</script>';
+    } else if (isset($_GET['size']) && $_GET['size'] == "error") {
+        echo '<script> alert("Adding Unsuccess")</script>';
+    }
+    if (isset($_GET['variation']) && $_GET['variation'] == "success") {
+        echo '<script> alert("Variation Successfully Added")</script>';
+    } else if (isset($_GET['variation']) && $_GET['variation'] == "error") {
+        echo '<script> alert("Adding Unsuccess")</script>';
+    }
+    ?>
+<?php include("app/includes/html/admin.foot.php");?>
 </body>
- 
 </html>
