69386b1b6c2120f91318532c0f487132d0d7a5a7
dominic-myb <desguerra246@gmail.com>
2024-01-30 16:55:17 +0800

    Remove comments

--- app/includes/html/admin.sidebar.php
+++ app/includes/html/admin.sidebar.php
@@ -10,12 +10,8 @@
     <a href="./index.php"><i class="fa fa-home"></i> Dashboard</a>
     <a href="#customers" onclick="showCustomers()"><i class="fa fa-users"></i> Customers</a>
     <a href="#category" onclick="showCategory()"><i class="fa fa-th-large"></i> Category</a>
-    <!-- <a href="#sizes"   onclick="showSizes()" ><i class="fa fa-th"></i> Sizes</a>
-    <a href="#productsizes"   onclick="showProductSizes()" ><i class="fa fa-th-list"></i> Product Sizes</a>     -->
     <a href="#products" onclick="showProductItems()"><i class="fa fa-th"></i> Products</a>
     <a href="#orders" onclick="showOrders()"><i class="fa fa-list"></i> Orders</a>
-
-    <!---->
 </div>
 
 <div id="main">
