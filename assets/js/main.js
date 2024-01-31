9e7851146c608ce8800991d4471330647e0fcb12
dominic-myb <desguerra246@gmail.com>
2024-01-31 21:05:29 +0800

    Added user auth. in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,42 @@
 $(document).ready(function () {
+  function updateAccountDropdown(isAuthenticated) {
+    var accountDropdown = $(".account-drp");
+    var loginLink = '<a href="login.php">Login</a>';
+    var signupLink = '<a href="form.php">Sign Up</a>';
+    var accountSettingsLink = '<a href="#">Account Settings</a>';
+    var logoutLink = '<a href="app/process/logout.php" style="color:red;">Logout</a>';
+  
+    if (!isAuthenticated) {
+      // User not authenticated
+      var cartDropdown = $(".cart-drp");
+      cartDropdown.off("mouseenter");
+      cartDropdown.find(".cart-drp-content").hide();
+  
+      accountDropdown.find(".account-btn").text("Account");
+      accountDropdown.find(".account-drp-content").html(loginLink + signupLink);
+    } else {
+      // User authenticated
+      accountDropdown.find(".account-btn").text("Welcome, "+jsUsername);
+      accountDropdown.find(".account-drp-content").html(accountSettingsLink + logoutLink);
+    }
+  }
+  
+  function checkUserSession() {
+    $.ajax({
+      url: "app/process/session.php",
+      method: "GET",
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+        var isAuthenticated = response && response.isAuthenticated;
+        updateAccountDropdown(isAuthenticated);
+      },
+      error: function () {
+        console.error("Error checking user session");
+      },
+    });
+  }
+  checkUserSession();  
   $("#register").on("click", function () {
     if (validateForm()) {
       $("#register-form").submit();
@@ -9,8 +47,8 @@ $(document).ready(function () {
     var name = $("#name").val();
     var email = $("#email").val();
     var phone = $("#phone").val();
-    var username = $("username").val();
-    var password = $("password").val();
+    var username = $("#username").val();
+    var password = $("#password").val();
 
     if (
       name.trim() === "" ||
