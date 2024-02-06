2b1bdae6d3705930f4800a24094883a1a5dd42ec
dominic-myb <desguerra246@gmail.com>
2024-02-06 19:14:20 +0800

    added form action link and added show pass func

--- form.php
+++ form.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include("app/includes/components/connection.php");
-include("app/process/form.submit.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,12 +13,14 @@ include("app/includes/html/form.head.php");
 <body><br><br><br>
   <div class="container">
     <h2>Create an Account</h2>
-    <form id="register-form" method="post">
+    <form id="register-form" action="app/process/form.submit.php" method="post">
       <input type="text" id="name" name="name" placeholder="Fullname" required>
       <input type="text" id="email" name="email" placeholder="sample@email.com" required>
       <input type="text" id="phone" name="phone" placeholder="Phone Number" required>
       <input type="text" id="username" name="username" placeholder="Username" required>
       <input type="password" id="password" name="password" placeholder="Password" required>
+      <label for="showPassword" class="form-check-label" style="display: inline-block; float:left; ">Show Password: &nbsp;&nbsp;&nbsp;</label>
+      <input type="checkbox" id="showPassword" class="form-check-input">
       <br>
       <input type="submit" id="register" value="REGISTER">
     </form>
