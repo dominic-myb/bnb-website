6c9bf80f85a9a006e3faecd4934d786313a10a8f
dominic-myb <desguerra246@gmail.com>
2024-02-06 19:18:52 +0800

    removed links

--- app/includes/html/admin.head.php
+++ app/includes/html/admin.head.php
@@ -1,11 +1,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./assets/css/admin.style.css">
-    </link>
-    <link rel="icon" type="image/png" href="img/tab-icon.png">
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap-5.2.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/admin.style.css"></link>
+    <link rel="icon" type="image/png" href="assets/images/img/tab-icon.png">
     <title>Café BLK & BRWN
         <?php $title ?>
     </title>
